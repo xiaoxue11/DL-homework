4f6505557b8a225b210bfb22531dd5b6c4d79ad8
xiaoxue11 <xuezhang0111@gmail.com>
2019-06-07 22:47:22 -0500

    deep learning homework in jupyter notebook

---
+++ 5_Sequence_Model/week2/Word Vector Repressiontation/test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  3 14:57:46 2019
+
+@author: 29132
+"""
+import numpy as np
+X="Morrocan couscous is my favorite dish"
+words=(X.lower()).split(' ')
+avg=np.zeros([len(words),1])
+
+
+    
+
